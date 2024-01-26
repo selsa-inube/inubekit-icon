39869ab4e4d766f5d5f6ac260bd6e0a8388904f6
Andres Babativa <carlos3k11@gmail.com>
2024-01-25 19:57:52 -0500

    feat(Icon): add Icon component and its related files

---
+++ src/Icon/index.tsx
@@ -0,0 +1,46 @@
+import { StyledIcon } from "./styles";
+import { Spacing, Variant, Shape, Appearance } from "./props";
+
+export interface IIconProps {
+  appearance: Appearance;
+  cursorHover?: boolean;
+  parentHover?: boolean;
+  icon: React.ReactNode;
+  disabled?: boolean;
+  spacing?: Spacing;
+  variant?: Variant;
+  shape?: Shape;
+  size?: string;
+  onClick?: () => void;
+}
+
+export const Icon = (props: IIconProps) => {
+  const {
+    appearance,
+    cursorHover,
+    parentHover,
+    icon,
+    disabled,
+    spacing,
+    variant,
+    shape,
+    size,
+    onClick,
+  } = props;
+
+  return (
+    <StyledIcon
+      appearance={appearance}
+      cursorHover={cursorHover}
+      parentHover={parentHover}
+      disabled={disabled}
+      spacing={spacing}
+      variant={variant}
+      shape={shape}
+      size={size}
+      onClick={disabled ? undefined : onClick}
+    >
+      {icon}
+    </StyledIcon>
+  );
+};
