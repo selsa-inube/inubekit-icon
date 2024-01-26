0f7a44e0b02ad6e455b39768af1d0cac3546d078
Andres Babativa <carlos3k11@gmail.com>
2024-01-25 19:58:58 -0500

    feat(Icon): add Icon component and stories

---
+++ src/Icon/Icon.stories.tsx
@@ -0,0 +1,44 @@
+import { ThemeProvider } from "styled-components";
+import { MdAdb } from "react-icons/md";
+
+import { Icon, IIconProps } from ".";
+
+import { props, parameters } from "./props";
+import { presente } from "@inubekit/foundations";
+
+const story = {
+  title: "data/Icon",
+  component: Icon,
+  parameters,
+  argTypes: props,
+};
+
+export const Default = (args: IIconProps) => <Icon {...args} />;
+
+Default.args = {
+  appearance: "primary",
+  icon: <MdAdb />,
+  cursorHover: false,
+  parentHover: false,
+  disabled: false,
+  spacing: "wide",
+  variant: "none",
+  shape: "rectangle",
+  size: "24px",
+};
+
+const theme = {
+  ...presente,
+};
+
+export const Themed = (args: IIconProps) => (
+  <ThemeProvider theme={theme}>
+    <Icon {...args} />
+  </ThemeProvider>
+);
+
+Themed.args = {
+  ...Default.args,
+};
+
+export default story;
