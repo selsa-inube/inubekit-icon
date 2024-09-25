7a5112d0b0f4aa01aeb3400b08d4029434e49380
cmarin001 <cmarin6051@gmail.com>
2024-09-25 11:52:18 -0500

    chore: move icon tokens from foundations to component

---
+++ src/Icon/Tokens/tokens.ts
@@ -0,0 +1,190 @@
+import { inube } from "@inubekit/foundations";
+
+const tokens = {
+  primary: {
+    content: {
+      color: {
+        regular: inube.palette.blue.B400,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.blue.B300,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.blue.B400,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.blue.B300,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  success: {
+    content: {
+      color: {
+        regular: inube.palette.green.G400,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.green.G300,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.green.G400,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.green.G300,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  warning: {
+    content: {
+      color: {
+        regular: inube.palette.yellow.Y400,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.yellow.Y300,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.yellow.Y400,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.yellow.Y300,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  danger: {
+    content: {
+      color: {
+        regular: inube.palette.red.R400,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.red.R300,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.red.R400,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.red.R300,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  help: {
+    content: {
+      color: {
+        regular: inube.palette.purple.P400,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.purple.P300,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.purple.P400,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.purple.P300,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  dark: {
+    content: {
+      color: {
+        regular: inube.palette.neutral.N900,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N500,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.neutral.N900,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.neutral.N500,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N10,
+      },
+    },
+  },
+  gray: {
+    content: {
+      color: {
+        regular: inube.palette.neutral.N300,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N100,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.neutral.N300,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.neutral.N100,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N900,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N900,
+      },
+    },
+  },
+  light: {
+    content: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N0,
+      },
+    },
+    background: {
+      color: {
+        regular: inube.palette.neutral.N10,
+        disabled: inube.palette.neutral.N20,
+        hover: inube.palette.neutral.N0,
+      },
+    },
+    contrast: {
+      color: {
+        regular: inube.palette.neutral.N900,
+        disabled: inube.palette.neutral.N70,
+        hover: inube.palette.neutral.N900,
+      },
+    },
+  },
+};
+
+export { tokens };
